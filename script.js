a8be8af90a4bb20da201327032e5f7b153354300
phoeberaw25 <phoebe.1999@hotmail.co.uk>
2021-05-28 15:52:13 +0100

    added weather icon

--- script.js
+++ script.js
@@ -36,6 +36,9 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+    
+    let iconElement = document.querySelector("#icon");
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
 
 function searchCity(city) {
