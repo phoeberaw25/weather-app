88a4925df4d54c707346f035d56bf2e6b79c5621
phoeberaw25 <phoebe.1999@hotmail.co.uk>
2021-05-28 16:05:18 +0100

    added humidity and wind speed

--- script.js
+++ script.js
@@ -38,7 +38,11 @@ function displayWeather(response) {
     response.data.weather[0].main;
     
     let iconElement = document.querySelector("#icon");
-iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt",  response.data.main.temp);
+
+document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
 }
 
 function searchCity(city) {
